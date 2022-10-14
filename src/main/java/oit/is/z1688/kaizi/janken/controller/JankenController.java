07bd74526ef8c0f71f9baaef0f5b2b7b672a6223
e1b20032 <e1b20032@oit.ac.jp>
2022-10-14 21:20:22 +0900

    ユーザーの追加

--- src/main/java/oit/is/z1688/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1688/kaizi/janken/controller/JankenController.java
@@ -1,7 +1,11 @@
 package oit.is.z1688.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JankenController {
@@ -11,4 +15,28 @@ public class JankenController {
     return "janken.html";
   }
 
+  /**
+   * POSTを受け付ける場合は@PostMappingを利用する /sample25へのPOSTを受け付けて，FormParamで指定された変数(input
+   * name)をsample25()メソッドの引数として受け取ることができる
+   *
+   * @param kakeru1
+   * @param kakeru2
+   * @param model
+   * @return
+   */
+  @PostMapping("/janken")
+  public String jankenpost(@RequestParam String name, ModelMap model) {
+    model.addAttribute("yourname", name);
+    return "janken.html";
+  }
+
+  @GetMapping("/jankengame")
+  public String janken(@RequestParam String hand, ModelMap model) {
+    String yourhand = hand;
+    String cpuHand = "Gu";
+    model.addAttribute("yourhand", yourhand);
+
+    return "janken.html";
+
+  }
 }
--- src/main/java/oit/is/z1688/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z1688/kaizi/janken/model/Janken.java
@@ -2,13 +2,17 @@ package oit.is.z1688.kaizi.janken.model;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Component;
+
+
 /**
  * Spring BootでModelとして扱われるクラス フレームワークがフィールドに値を代入したり，取得したりするため，getter/setterが必要
  */
-
+@Component
 public class Janken {
   ArrayList<Double> numList = new ArrayList<>();
   double ave;
+
 
   // コンストラクタ
   public Janken(ArrayList<Double> numList) {
